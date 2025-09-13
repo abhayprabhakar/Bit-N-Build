f00e5765f371e21e7a7560a03131dea74b71e7b9
Abhay Prabhakar <123283257+abhayprabhakar@users.noreply.github.com>
2025-09-13 13:48:29 +0530

    added dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from './assets/components/sign-in/SignIn';
+import Dashboard from './assets/components/dashboard/Dashboard';
 
 function App() {
    return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
+  <Route path="/" element={<Dashboard />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
